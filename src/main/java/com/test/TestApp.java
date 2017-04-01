83797a068ac1a29383fdba6c19555b6e305d0c89
yanming <yanming@pxsj.com>
2017-04-01 13:41:41 +0800

    test-b-c

---
+++ src/main/java/com/test/TestApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test;
+
+/***
+ * <p>
+ * @author LingMin 
+ * @date 2017Äê4ÔÂ1ÈÕ<br>
+ * @version 1.0<br>
+ */
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("²âÊÔ");
+	}
+
+}
