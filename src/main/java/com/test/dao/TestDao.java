ace23fb46fc9adf40afa1a6957b405f5e5f230ce
yanming <yanming@pxsj.com>
2017-04-01 13:57:04 +0800

    测试提交

---
+++ src/main/java/com/test/dao/TestDao.java
@@ -0,0 +1,18 @@
+/**
+ * &lt;p&gt;
+ * copyright &amp;copy;  2015, all rights reserved.
+ * @author Administrator
+ * @version $Id$
+ * @since 1.0
+ * 
+ */
+package com.test.dao;
+
+/** <p>
+ * @author LingMin 
+ * @date 2017Äê4ÔÂ1ÈÕ<br>
+ * @version 1.0<br>
+ */
+public interface TestDao {
+
+}
